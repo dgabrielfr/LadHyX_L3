0192c9516b48e41acc831b1d93f789ee535b2371
Damien GABRIEL <damien.gabriel.fr@gmail.com>
2016-07-01 01:30:42 +0200

    Ajout du début du code c++ du stage

---
+++ code.cpp
@@ -0,0 +1,19 @@
+// Déclaration des librairies standard c++
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+// Déclaration des librairies propres ChannelFlow
+#include "channelflow/chebyshev.h"
+#include "channelflow/flowfield.h"
+#include "channelflow/dns.h"
+#include "channelflow/turbstats.h"
+#include "channelflow/periodicfunc.h"
+
+using namespace std;
+using namespace channelflow;
+
+int main(){
+  cout << setprecision(17);
+}
